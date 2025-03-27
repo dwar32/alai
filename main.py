7c46fcf260c7172f8f413bda512ac0723f254037
dwar32 <kulm.alisher@gmail.com>
2025-03-27 03:38:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def get_sheet_data():
                "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30",
                "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41",
                "Остаток", "56"]
-    data = sheet.get_all_records(head=2, expected_headers=headers)
+    data = sheet.get_all_records(head=2)
     return pd.DataFrame(data)
 
 def extract_article(text):
